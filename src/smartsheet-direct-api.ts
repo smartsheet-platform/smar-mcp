ccc48847ac9c57a270b64baf336d066db807391b
Imran Khawaja <imran.khawaja@smartsheet.com>
2025-04-19 12:41:41 -0700

    chore: add a header to indicate source of calls

--- src/smartsheet-direct-api.ts
+++ src/smartsheet-direct-api.ts
@@ -63,7 +63,8 @@ export class SmartsheetDirectAPI {
           data,
           headers: {
             'Authorization': `Bearer ${this.accessToken}`,
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'x-smar-external-client': 'mcp',
           }
         });
         
